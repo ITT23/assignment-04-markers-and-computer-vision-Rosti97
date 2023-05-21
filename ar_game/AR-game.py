8c9f461412ab06ea207f4421657738c21c5b01d5
Sabrina Hößl <das.rosti@gmail.com>
2023-05-21 17:28:15 +0200

    still no idea

--- ar_game/AR-game.py
+++ ar_game/AR-game.py
@@ -42,6 +42,7 @@ class ArucoDetector():
         self.corners = []
         self.aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
         self.aruco_params = aruco.DetectorParameters()
+        self.detected = False
 
     def setFrame(self, frame):
         self.frame = frame
@@ -75,32 +76,80 @@ class ArucoDetector():
             for c in corners:
                 self.corners.append([c[0][0][0], c[0][0][1]])
             
-            print(f"Length: {len(self.corners)}")
+            #print(f"Length: {len(self.corners)}")
 
             orderd_stuff = self.order_points(np.array(self.corners))
 
             if len(self.corners) == 4:
-                print(".")
+                # print(".")
                 selection_points = np.float32(orderd_stuff)
                 warp_points = np.float32([[0,0], [WINDOW_WIDTH,0], [WINDOW_WIDTH, WINDOW_HEIGHT], [0,WINDOW_HEIGHT]])
 
-                print(selection_points)
-                print(warp_points)
-                print("--")
+                # print(selection_points)
+                # print(warp_points)
+                # print("--")
 
 
                 matrix = cv2.getPerspectiveTransform(selection_points, warp_points)
                 img_warp = cv2.warpPerspective(frame, matrix, (WINDOW_WIDTH, WINDOW_HEIGHT),flags=cv2.INTER_LINEAR)
 
                 self.frame = img_warp
+                self.detected = True
 
             else:
                 self.frame = frame
+                self.detected = False
             
             self.corners.clear()
 
         else:
             self.frame = frame 
+
+
+class ContourDetector():
+
+
+    # Rectangle 
+    # Wir gehen für jeden contours in Contours durch
+    # ob x/y zwischen x/y von Rectangle liegt
+
+
+    def __init__(self) -> None:
+        self.threshold = 100
+        self.out = None
+        pass
+
+    def set_frame(self, frame):
+        self.frame = frame
+
+    def measure_distance(self, x1:int, y1:int, x2:int, y2:int) -> float:
+        """measures the distance between two coordinates"""
+        distance = np.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+        return distance
+
+    def detect_collision(self, frame, t):
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        ret, tresh = cv2.threshold(gray, self.threshold, 255, cv2.THRESH_BINARY_INV)
+
+        contours, hierachy = cv2.findContours(tresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        img_contours = cv2.drawContours(frame, contours, -1, (255,0,0), 3)
+
+        test_array = []
+
+        for contour in contours:
+            x = contour[0][0][0]
+            y = contour[0][0][1]
+
+            if x != 0 and x >= 400 and x <= 600 and y>=410:
+              t.color = (0,0,255)
+              print(x)
+
+
+
+        self.out =  img_contours
+
 
 cap = cv2.VideoCapture(video_id)
 
@@ -109,16 +158,20 @@ WINDOW_HEIGHT = 480
 
 window = pyglet.window.Window(WINDOW_WIDTH, WINDOW_HEIGHT)
 detector = ArucoDetector()
+contourd = ContourDetector()
 
 @window.event
 def on_show():
     ret, frame = cap.read()
     detector.setFrame(frame)
+    contourd.set_frame(frame)
     
 
 @window.event
 def on_draw():
     window.clear()
+
+    t = pyglet.shapes.Rectangle(x=400, y= 410, width=50, height=50, color=(50,225,30))
     ret, frame = cap.read()
     #img = cv2glet(frame, 'BGR')
 
@@ -126,8 +179,14 @@ def on_draw():
 
     detector.detect_markers(frame)
     test = detector.getFrame()
-    img = cv2glet(test, 'BGR')
 
+    if detector.detected:
+        contour_test = contourd.detect_collision(test, t)
+        img = cv2glet(contourd.out, 'BGR')
+    else:
+        img = cv2glet(test, 'BGR')
+
+    
     
 
     # Convert the frame to grayscale
@@ -154,5 +213,5 @@ def on_draw():
         print("ups")
 
     img.blit(0, 0, 0)
-
+    t.draw()
 pyglet.app.run()

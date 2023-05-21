c28e2210b50ce108c34654beccf3bc5587419ec0
Sabrina Hößl <das.rosti@gmail.com>
2023-05-21 13:18:16 +0200

    i have no idea what i'm doing

--- ar_game/AR-game.py
+++ ar_game/AR-game.py
@@ -12,8 +12,8 @@ if len(sys.argv) > 1:
     video_id = int(sys.argv[1])
 
 # define aruco dictionary and parameters
-aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
-aruco_params = aruco.DetectorParameters()
+# aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
+# aruco_params = aruco.DetectorParameters()
 
 # converts OpenCV image to PIL image and then to pyglet texture
 # https://gist.github.com/nkymut/1cb40ea6ae4de0cf9ded7332f1ca0d55
@@ -36,52 +36,118 @@ def cv2glet(img,fmt):
                                    pitch=top_to_bottom_flag*bytes_per_row)
     return pyimg
 
-def detectMarkers(img):
-  corners, ids, rejectedImgPoints = aruco.detectMarkers(img, aruco_dict, parameters=aruco_params)
+class ArucoDetector():
+
+    def __init__(self) -> None:
+        self.corners = []
+        self.aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
+        self.aruco_params = aruco.DetectorParameters()
+
+    def setFrame(self, frame):
+        self.frame = frame
+
+    def getFrame(self):
+        return self.frame
+    
+    def order_points(self, pts):
+        # desperate hour long google search:
+        # https://pyimagesearch.com/2016/03/21/ordering-coordinates-clockwise-with-python-and-opencv/
+        # i feel dumb for not knowing any other solution ups
+        rect = np.zeros((4, 2), dtype = "float32")
+
+        s = pts.sum(axis = 1)
+        rect[0] = pts[np.argmin(s)]
+        rect[2] = pts[np.argmax(s)]
+
+        diff = np.diff(pts, axis = 1)
+        rect[1] = pts[np.argmin(diff)]
+        rect[3] = pts[np.argmax(diff)]
+
+        return rect
+
+    def detect_markers(self, frame):
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, self.aruco_dict, parameters=self.aruco_params)
+
+        if ids is not None:
+            aruco.drawDetectedMarkers(frame, corners)
+
+            for c in corners:
+                self.corners.append([c[0][0][0], c[0][0][1]])
+            
+            print(f"Length: {len(self.corners)}")
+
+            orderd_stuff = self.order_points(np.array(self.corners))
+
+            if len(self.corners) == 4:
+                print(".")
+                selection_points = np.float32(orderd_stuff)
+                warp_points = np.float32([[0,0], [WINDOW_WIDTH,0], [WINDOW_WIDTH, WINDOW_HEIGHT], [0,WINDOW_HEIGHT]])
+
+                print(selection_points)
+                print(warp_points)
+                print("--")
 
 
-  # Check if marker is detected
-  if ids is not None:
-      # Draw lines along the sides of the marker
-      aruco.drawDetectedMarkers(img, corners)
+                matrix = cv2.getPerspectiveTransform(selection_points, warp_points)
+                img_warp = cv2.warpPerspective(frame, matrix, (WINDOW_WIDTH, WINDOW_HEIGHT),flags=cv2.INTER_LINEAR)
 
-  # Display the frame
-  cv2.imshow('frame', img)
+                self.frame = img_warp
 
-  # Wait for a key press and check if it's the 'q' key
-  if cv2.waitKey(1) & 0xFF == ord('q'):
-      print("exit")
+            else:
+                self.frame = frame
+            
+            self.corners.clear()
 
-# Create a video capture object for the webcam
+        else:
+            self.frame = frame 
+
 cap = cv2.VideoCapture(video_id)
 
 WINDOW_WIDTH = 640
 WINDOW_HEIGHT = 480
 
 window = pyglet.window.Window(WINDOW_WIDTH, WINDOW_HEIGHT)
+detector = ArucoDetector()
+
+@window.event
+def on_show():
+    ret, frame = cap.read()
+    detector.setFrame(frame)
+    
 
 @window.event
 def on_draw():
     window.clear()
     ret, frame = cap.read()
-    img = cv2glet(frame, 'BGR')
+    #img = cv2glet(frame, 'BGR')
 
     ret, frame = cap.read()
 
+    detector.detect_markers(frame)
+    test = detector.getFrame()
+    img = cv2glet(test, 'BGR')
+
+    
+
     # Convert the frame to grayscale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Detect ArUco markers in the frame
-    corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=aruco_params)
+    # corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=aruco_params)
+
+    #test = np.array(ids)
+    
 
     # Check if marker is detected
-    if ids is not None:
-        # Draw lines along the sides of the marker
-        aruco.drawDetectedMarkers(frame, corners)
+    # if ids is not None:
+    #     # Draw lines along the sides of the marker
+    #     aruco.drawDetectedMarkers(frame, corners)
 
-    # Display the frame
-    #cv2.imshow('frame', frame)
-    img = cv2glet(frame, 'BGR')
+    # else:
+    #     # Display the frame
+    #     #cv2.imshow('frame', frame)
+    #     img = cv2glet(frame, 'BGR')
 
     # Wait for a key press and check if it's the 'q' key
     if cv2.waitKey(1) & 0xFF == ord('q'):
